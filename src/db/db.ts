edc64119a51af226ca5585d3e55014a7a4ed60f3
deekshith <deekshith@Mac-mini.local>
2023-08-26 12:07:21 +0530

    updated DB URL

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,9 @@
+import { config } from "dotenv";
 import mongoose from "mongoose";
+config();
 
 export const dbConnection = mongoose.createConnection(
-  "mongodb://127.0.0.1:27017/",
+  process.env.MONGO_URL || "mongodb://127.0.0.1:27017/",
   {
     dbName: "imageUploader",
   }
